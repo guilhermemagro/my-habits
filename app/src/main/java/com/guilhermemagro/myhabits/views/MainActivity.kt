f21e280dc074b78f0177e71aa0f19ffa8c3eda25
GuilhermeRM <guilherme.magro@hotmail.com>
2020-12-23 22:43:22 -0300

    First commit

---
+++ app/src/main/java/com/guilhermemagro/myhabits/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.guilhermemagro.myhabits.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.guilhermemagro.myhabits.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
